3e9c2efd0875b11d0b4289495d275a5e9fc332d2
test <shiinatan424@gmail.com>
2024-03-01 19:06:16 +0900

    test

--- src/hook/uiohook_helper_win.cpp
+++ src/hook/uiohook_helper_win.cpp
@@ -73,9 +73,8 @@ static void logger_proc(unsigned int level, void *, const char *format, va_list 
 {
     switch (level) {
     default:
-    case LOG_LEVEL_DEBUG:
     case LOG_LEVEL_INFO:
-        blogva(LOG_DEBUG, format, args);
+        blogva(LOG_INFO, format, args);
         break;
     case LOG_LEVEL_WARN:
     case LOG_LEVEL_ERROR:
